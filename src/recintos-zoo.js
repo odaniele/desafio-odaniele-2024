4929069ef1ff445d409f95d64143f76433c0ab29
Daniele Oliveira <daniele.oliveiras@hotmail.com>
2024-09-12 18:47:46 -0300

    definindo objetos

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,6 +1,77 @@
+const recintos = [
+{
+    numero: 1, 
+    bioma: 'savana',
+    tamanhoTotal: 10,
+    animal: 'macaco',
+    animaisExistentes: 3,
+},
+{
+    numero: 2,
+    bioma: 'floresta',
+    tamanhoTotal: 5,
+    animal: '',
+    animaisExistentes: '',
+},
+{
+    numero: 3,
+    bioma: 'savanaRio',
+    tamanhoTotal: 7,
+    animal: 'gazela',
+    animaisExistentes: 1
+},
+{
+    numero: 4,
+    bioma: 'rio',
+    tamanhoTotal: 8,
+    animal: '',
+    animaisExistentes: ''
+},
+{
+    numero: 5,
+    bioma: 'savana',
+    tamanhoTotal: 9,
+    animal: 'leao',
+    animaisExistentes: 1
+}
+];
+
+const animais = [
+{
+    especie: 'LEAO',
+    tamanho: 3,
+    bioma: 'savana'
+},
+{
+    especie: 'LEOPARDO',
+    tamnho: 2,
+    bioma: 'savana'
+},
+{
+    especie: 'CROCODILO',
+    tamanho: 3,
+    bioma: 'rio'
+},
+{
+    especie: 'MACACO',
+    tamanho: 1,
+    bioma: 'savana' || 'floresta'
+},
+{
+    especie: 'GAZELA',
+    tamanho: 2,
+    bioma: 'savana'
+},
+{
+    especie: 'HIPOPOTAMO',
+    tamanho: 4,
+    bioma: 'savana' || 'rio'
+}    
+];
 class RecintosZoo {
 
     analisaRecintos(animal, quantidade) {
+
     }
 
 }
